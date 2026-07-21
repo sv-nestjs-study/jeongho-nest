19e9d9cecab6fc162e4591f0d29ec49d5add2645
jeongho0406 <kjh@skyventures.co.kr>
2026-07-22 08:50:32 +0900

    feat: post 연동
    - post api 연동
    - validata & pipe 설정 추가

--- apps/api/src/posts/posts.controller.ts
+++ apps/api/src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+}
--- apps/api/src/posts/posts.service.ts
+++ apps/api/src/posts/posts.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from './dto/create-post.dto';
+import { Post } from './entities/post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postsRepository: Repository<Post>,
+  ) {}
+
+  create(createPostDto: CreatePostDto): Promise<Post> {
+    const post = this.postsRepository.create(createPostDto);
+
+    return this.postsRepository.save(post);
+  }
+}
